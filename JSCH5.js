168fef78ce16e5be8edee61792b66edc2b348022
M S I <M S I@MSI>
2023-06-18 18:47:29 +0700

    first commit

---
+++ JSCH5.js
@@ -0,0 +1,5 @@
+video.addEventListener("click", function video() { alert("ini video Jedag Jedug Ala Bible"); });
+audio.addEventListener("click", function audio() { alert("ini Audio Jedag Jedug Ala Bible"); });
+gambar.addEventListener("click", function gambar() {
+    alert("ini gambar Dewa 19");
+});
